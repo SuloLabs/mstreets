f82d79fdea974aa7844b9582679b57cc54dc5c67
Jeffrey Scott Martin - Sulo Labs <sulo.labs@gmail.com>
2021-02-23 11:22:58 -0500

    added dev name

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#c84135`,
       marginBottom: `1.45rem`,
     }}
   >
