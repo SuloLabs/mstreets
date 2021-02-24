8b73b67d6a4d70d196d0624db0d04359976f5f62
Jeffrey Scott Martin - Sulo Labs <sulo.labs@gmail.com>
2021-02-24 14:06:08 -0500

    added carousel with descriptions

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 import Carousel from "react-bootstrap/Carousel"
+import Button from "react-bootstrap/Button"
 
 const IndexPage = ({data}) => (
   <Layout>
@@ -14,12 +15,17 @@ const IndexPage = ({data}) => (
     <Carousel>
       {data.carouselPhotos.edges.map(image => (
         <Carousel.Item key={image.node.id}>
-          <Img
-            
+          <Img style={{maxHeight: '90vh'}}
             fluid={image.node.childImageSharp.fluid}
             alt={image.node.base.split('-').join(' ').split('.')[0]}
           />
-        </Carousel.Item>
+          <Carousel.Caption>
+            <h2>
+            {image.node.base.split('-').join(' ').split('.')[0]}
+            {' '}<Button href="#">Learn More</Button>
+            </h2>
+          </Carousel.Caption>
+        </Carousel.Item>  
         
       ))}
     </Carousel>
