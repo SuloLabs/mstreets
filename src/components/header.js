c02055507d155844e4427f5cdc7f17cd6a7626af
Jeffrey Scott Martin - Sulo Labs <sulo.labs@gmail.com>
2021-02-24 11:45:46 -0500

    added carousel without discriptions

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,30 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Navbar from 'react-bootstrap/Navbar';
+//import NavDropdown from 'react-bootstrap/NavDropdown';
+import Nav from 'react-bootstrap/Nav';
+import container from 'react-bootstrap/Container';
+import Container from "react-bootstrap/Container";
+//import Button from 'react-bootstrap/Button';
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `#c84135`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    <Navbar bg="primary" expand="md" variant="dark" fixed="top">
+      <Container>
+        <Navbar.Brand href="/">{siteTitle}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarResponsive" />
+        <Navbar.Collapse id="navbarResponsive">
+          <Nav as="ul" className="ml-auto">
+            <Nav.Item as="li">
+              <Link to="/page-2" className="nav-link" activeClassName="active">
+                Page 2
+              </Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   </header>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,49 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+import Carousel from "react-bootstrap/Carousel"
+
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to the new Malachi S.T.R.E.E.T.S development site.</p>
-    <p>Now we go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <Carousel>
+      {data.carouselPhotos.edges.map(image => (
+        <Carousel.Item key={image.node.id}>
+          <Img
+            
+            fluid={image.node.childImageSharp.fluid}
+            alt={image.node.base.split('-').join(' ').split('.')[0]}
+          />
+        </Carousel.Item>
+        
+      ))}
+    </Carousel>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
 export default IndexPage
+
+export const pageQuery = graphql `
+  query {
+    carouselPhotos: allFile(filter: {relativeDirectory: {eq: "carousel"}, extension: {regex: "/(jpg)|(png)|(jpg)/"}}) {
+      edges {
+        node {
+            id
+            base
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+        }
+      }
+    }
+  }
+`
